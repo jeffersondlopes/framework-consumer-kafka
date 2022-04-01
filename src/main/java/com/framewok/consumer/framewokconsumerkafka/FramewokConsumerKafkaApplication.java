7cef326152e9ef0f9a8c651b3db398f6235fefbd
jefferson <jefferson.diego.e@riachuelo.com.br>
2022-04-01 18:56:27 -0300

    first commit.

---
+++ src/main/java/com/framewok/consumer/framewokconsumerkafka/FramewokConsumerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.framewok.consumer.framewokconsumerkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramewokConsumerKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FramewokConsumerKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/framewok/consumer/framewokconsumerkafka/FramewokConsumerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framewok.consumer.framewokconsumerkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramewokConsumerKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
